e5e05853a788ba071ff62d479a6a27544ca24869
Det Yang <detyang22@gmail.com>
2019-07-29 16:24:07 -0500

    072919_3

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/user/<name>')
-def user_page(name):
-	return '<h1>Hello User: %s</h1><img src="http://helloflask.com/totoro.gif">' %name
+name = 'Det Yang'
+movies = [
+    {'title': 'Titanic', 'year': '1997'},
+    {'title': 'The Prestige', 'year': '2006'},
+    {'title': 'Nightcrawler', 'year': '2014'},
+    {'title': 'The Secret Life of Walter Mitty', 'year': '2013'},
+    {'title': 'Three Billboards', 'year': '2018'},
+    {'title': 'Up', 'year': '2009'},
+    {'title': 'The Hateful Eight', 'year': '2015'},
+    {'title': 'Big Fish', 'year': '2003'},
+    {'title': 'The Sixth Sense', 'year': '1999'}
+]
+
+@app.route('/')
+def index():
+        return render_template('index.html', name=name, movies=movies)
+
